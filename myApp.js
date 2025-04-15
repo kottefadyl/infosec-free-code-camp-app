259a15c880676d200dfbb8c44445f311bc7ea985
kottefadyl <kottekevin01@gmail.com>
2025-04-15 23:22:12 +0100

    initial application

--- myApp.js
+++ myApp.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
+
+const PORT = process.env.PORT || 3030;
+app.listen(PORT, ()=>{
+  console.log(`Useful programmer info security app started on port ${PORT}` );
+  
+})
 
 
 
