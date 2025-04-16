f289dc815bd4828f856bd49ceb3fb89659a9440d
kottefadyl <kottekevin01@gmail.com>
2025-04-16 23:12:11 +0100

    Mitigate the Risk of Clickjacking with helmet.frameguard()

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ const helmet = require('helmet');
 const app = express();
 
 app.use(helmet.hidePoweredBy())
-
+ app.use(helmet.frameguard({action:'deny'}))
 helmet.hidePoweredBy()// here is the commend to remove the X-Powered-By: Express in the header off responses of the api which can be an indication or a guide that help hacker
 
 
