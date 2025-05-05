78937f631ddc2eb489842608ac9948d73bd0b74d
kottefadyl <kottekevin01@gmail.com>
2025-05-05 23:27:22 +0100

    Disable DNS Prefetching with helmet.dnsPrefetchControl()

--- myApp.js
+++ myApp.js
@@ -15,6 +15,9 @@ app.use(helmet.ieNoOpen())
 const timeInSeconds = 90 * 24 * 60 * 60
 app.use(helmet.hsts({maxAge:timeInSeconds, force: true}))
 
+// Désactiver le DNS Prefetching
+app.use(helmet.dnsPrefetchControl());
+
 
 
 
